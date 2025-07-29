2dc02cf9a6e02b2fc3cb813c7da96ef750bb7572
Roman Kolivoshko <romankolivoshko@gmail.com>
2025-07-30 00:15:58 +0200

    Create and modifaered contentview

--- WeatherWear/ContentView.swift
+++ WeatherWear/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cityName: String = ""
+    @State private var temperature: String = "--℃"
+    @State private var condition: String = ""
+    @State private var clothingAdvice: Array = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Weather")
+                .font(.largeTitle)
+                .bold()
+            
+            TextField("Enter the city name", text: $cityName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+            
+            Button("Schow the weather") {
+                fetchWeather(for: cityName)
+            }
+            
         }
-        .padding()
     }
 }
 
