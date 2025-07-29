4973c59fc48d60c9a53c53d8f9c2609deba2f4db
Roman Kolivoshko <romankolivoshko@gmail.com>
2025-07-29 21:30:30 +0200

    Initiaal commit - WeatherWearApp setup

---
+++ WeatherWear/WeatherWearApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWearApp.swift
+//  WeatherWear
+//
+//  Created by romankolivoshko on 29.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
